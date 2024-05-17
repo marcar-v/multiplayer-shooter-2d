400eaeada41df6071bb74d365845291c26df1180
marcar-v <vmarcar95@gmail.com>
2024-05-18 01:22:14 +0200

    jump added

--- Shooter 2D/Assets/Scripts/PlayerController.cs
+++ Shooter 2D/Assets/Scripts/PlayerController.cs
@@ -7,7 +7,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviourPun, IPunObservable
 {
     [SerializeField] PhotonView _photonView;
-    
+    [SerializeField] SpriteRenderer _playerSprite;
+
     [Header("Movement")]
     [SerializeField] float _speed = 10;
 
@@ -24,6 +25,7 @@ public class PlayerController : MonoBehaviourPun, IPunObservable
     private int _totalJumps = 1;
 
 
+
     void Awake()
     {
         _boxCollider2D = GetComponent<BoxCollider2D>();
@@ -36,7 +38,7 @@ public class PlayerController : MonoBehaviourPun, IPunObservable
         if (photonView.IsMine)
         {
             PlayerMovement();
-            PlayerJump();
+            JPlayerJump();
         }
         else
         {
@@ -48,26 +50,53 @@ public class PlayerController : MonoBehaviourPun, IPunObservable
     {
         Vector3 _movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0);
         transform.position += _movement * _speed * Time.deltaTime;
+
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            _playerSprite.flipX = true;
+            _photonView.RPC("OnDirectionChangeLeft", RpcTarget.Others);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            _playerSprite.flipX = false;
+            _photonView.RPC("OnDirectionChangeRight", RpcTarget.Others);
+        }
+
     }
+    [PunRPC]
+    void OnDirectionChangeLeft()
+    {
+        _playerSprite.flipX = true;
+    }
+    [PunRPC]
+    void OnDirectionChangeRight()
+    {
+        _playerSprite.flipX = false;
+    }
+
     bool isGrounded()
     {
         RaycastHit2D _raycastHit2D = Physics2D.BoxCast(_boxCollider2D.bounds.center, new Vector2(_boxCollider2D.bounds.size.x, _boxCollider2D.bounds.size.y), 0f, Vector2.down, 0.2f, _groundLayer);
         return _raycastHit2D.collider != null;
+
     }
 
-    void PlayerJump()
+    void JPlayerJump()
     {
         if (isGrounded())
         {
             _remainingJumps = _totalJumps;
         }
+
         if (Input.GetKeyDown(KeyCode.Space) && _remainingJumps > 0)
         {
+            _remainingJumps = _totalJumps;
             _remainingJumps--;
             _rb.velocity = new Vector2(_rb.velocity.x, 0f);
             _rb.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
         }
     }
+
     private void SmoothMovement()
     {
         transform.position = Vector3.Lerp(transform.position, _smoothMove, Time.deltaTime * _smoothTime);
