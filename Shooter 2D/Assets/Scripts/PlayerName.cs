cbe22f1dbe10ed5dc4c8aac51908326bd2a90db0
marcar-v <vmarcar95@gmail.com>
2024-05-18 11:00:26 +0200

    main menu fixed + player's name

---
+++ Shooter 2D/Assets/Scripts/PlayerName.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerName : MonoBehaviour
+{
+    [SerializeField] TMP_InputField _playerName;
+    [SerializeField] Button _setNameButton;
+
+    public void OnTFChange()
+    {
+
+    }
+}
